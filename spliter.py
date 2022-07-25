549fafc725a95769200fd91438e8f7546bb04b87
yugu0202 <applerin1119@gmail.com>
2022-07-25 11:43:09 +0900

    update spliter.py

--- spliter.py
+++ spliter.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from PIL import Image
+import random
 
 def Main():
     args = sys.argv
@@ -9,12 +10,18 @@ def Main():
     out_path = os.path.join(cwd,"splited")
 
     img = Image.open(os.path.join(cwd,args[1]))
-    dn = int(args[2])
+    dn = int(img.size[0] / 10)
 
     os.makedirs(out_path,exist_ok=True)
 
     for num,slice_img in enumerate(SplitImage(img,dn),1):
-        slice_img.save(out_path + "/" + str(num) +".png","PNG")
+        if num < 5 or num > dn-4:
+            continue
+        while True:
+            path = out_path + "/" + str(random.randint(0,100)) +".png"
+            if not os.path.isfile(path):
+                slice_img.save(path,"PNG")
+                break
 
     print("finish")
 
@@ -24,7 +31,6 @@ def SplitImage(img,dn):
     split_width = width / dn
     for w1 in range(dn):
         w2 = w1 * split_width
-        print(w2,40,split_width+w2,height-40)
         yield img.crop((w2,40,split_width+w2,height-40))
 
 if __name__ == "__main__":
